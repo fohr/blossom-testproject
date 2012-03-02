2cf442a9aaf1448c2d6826887ebe184e1a42b31c
Erich Ocean <erich.ocean@me.com>
2012-03-02 02:20:38 -0800

    clarified docs

--- my_app/core.js
+++ my_app/core.js
@@ -11,30 +11,35 @@
 //
 SC.Application.create(); // It automatically assigns itself to `SC.app`.
 
-// In general, you probably *don't* want to use `SC.app` as the namespace for 
-// your app's classes and objects. Instead, do something like:
-//
-//     var NS = global.NS = SC.Object.create(...); // Make `NS` our namespace.
-//     NS.Record = SC.Record.extend(...);
-//
+// You should create a namespace for the classes and global objects defined 
+// by your application.  In this case, we'll call it `MyApp`.
 var MyApp;
 MyApp = global.MyApp = SC.Object.create({
   name: "My App"
 });
 
-// Create a view surface for our user interface, with `contentView` set to
-// a custom subclass of `SC.View` that draws itself green.  In Blossom, 
-// "surfaces" are what are visible in the viewport, so you need to host a
-// view tree in a surface, in this case, an `SC.ViewSurface`, in order to 
-// actually display the view tree and receive events.
+// You'll need to create at least one surface that defines your user 
+// interface.  In Blossom, "surfaces" are what are visible in the viewport, 
+// so to draw any user interface widgets into the viewport like buttons and 
+// checkboxes, you'll need a surface to draw them into and receive events 
+// from.
+
+// The surface that hosts widgets and layers in Blossom is called 
+// `SC.View`. We'll create one and assign it to the `ui` property inside our 
+// `MyApp` namespace.
 MyApp.ui = SC.View.create();
 
+// Next you'll want to create a widget you can add to your `SC.View` surface 
+// instance. Widgets are a subclass of `SC.Layer` that provide their own 
+// behavior and event handling.  In this case, we're creating a custom 
+// widget.
 MyApp.widget = SC.Widget.create({
 
-  // This configures the location and size of our widget's drawing layer 
-  // within its View surface.  Here, we're telling Blossom we want a widget 
-  // that is 500px by 500px, and offset zero pixels from the center of its
-  // parent (in this case, the View surface is its parent).
+  // This configures the location and size of our widget within its 
+  // superlayer (or if it has none, it's surface).  Here, we're telling 
+  // Blossom we want a widget that is 500px by 500px, and offset zero pixels 
+  // from the center of its superlayer (or surface, if it doesn't have a 
+  // superlayer).
   layout: { width: 500, height: 500, centerX: 0, centerY: 0 },
 
   // These methods are automatically be invoked by Blossom when the mouse 
@@ -49,31 +54,32 @@ MyApp.widget = SC.Widget.create({
   mouseExited:  function(evt) { document.body.style.cursor = 'default'; },
 
   // This method is called when the layer needs to be re-cached, in the 
-  // process of being renderered to the View surface. The `context` parameter 
-  // is a CanvasRenderingContext2D. See http://www.w3.org/TR/2dcontext/ for 
-  // information on the drawing functions that are available.  This API is 
-  // compatible with all platforms supported by Blossom, including the native 
-  // runtimes.
+  // process of being composited into the `SC.View` surface. The `context` 
+  // parameter is a `CanvasRenderingContext2D` object. See 
+  // http://www.w3.org/TR/2dcontext/ for information on the drawing functions 
+  // that are available.  This API is compatible with all platforms supported 
+  // by Blossom, including the native runtimes.
   render: function(ctx) {
     ctx.fillStyle = '#859900'; // green
     ctx.fillRect(0, 0, ctx.width, ctx.height);
   }
 });
 
-// This function is called by Blossom when your app is ready to begin running.
+// The `main()` function is called by Blossom when your app is ready to begin 
+// running. You'll want to set your app's `ui` in this function.
 function main() {
   var ui = MyApp.ui,
       widget = MyApp.widget;
 
-  // Add the widget to our view's layers.  (Widgets are SC.Layer subclasses 
-  // with event handling behavior.)
+  // SC.View initially has no layers, so you'll add your widget to your 
+  // view's layers so it can display itself and receive events.
   ui.get('layers').pushObject(widget);
 
-  // Tell `SC.app` that we want our `ui` surface to becomes the app's user 
-  // interface ('ui').  Each app has only one active `ui` surface, though you 
-  // can change the global `ui` at any time.
-  //
+  // Next, you'll tell `SC.app` that you want your `SC.View` surface to 
+  // become the app's user interface ('ui').  Each app has only one active 
+  // `ui` surface, though you can change it at any time.
+  SC.app.set('ui', ui);
+
   // You can also add arbitary surfaces to the application using 
   // `SC.app.addSurface(...)`, but this is for a more advanced tutorial.
-  SC.app.set('ui', ui);
 }
